9c79603bc68e5a2692f32e7a4851de88ce6c358b
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2021-02-27 13:16:54 +0900

    회원가입 모달 스타일 초기 작업

--- components/auths/SignUpModal.tsx
+++ components/auths/SignUpModal.tsx
@@ -1,15 +1,70 @@
 import React from "react";
 import styled from "styled-components";
+import CloseXIcon from "../../public/static/svg/modal/modal_close_x_icon.svg";
+import MailIcon from "../../public/static/svg/auth/mail.svg";
+import PersonIcon from "../../public/static/svg/auth/person.svg";
+import OpenedEyeIcon from "../../public/static/svg/auth/opened_eye.svg";
+import ClosedEyeIcon from "../../public/static/svg/auth/closed_eye.svg";
+import palette from "../../styles/palette";
 
-const Container = styled.div`
+const Container = styled.form`
   width: 568px;
-  height: 614px;
+  padding: 32px;
   background-color: white;
   z-index: 11;
+
+  .modal-close-x-icon {
+    cursor: pointer;
+    display: block;
+    margin: 0 0 40px auto;
+  }
+
+  .input-wrapper {
+    position: relative;
+    margin-bottom: 16px;
+    input {
+      position: relative;
+      width: 100%;
+      height: 46px;
+      padding: 0 44px 0 11px;
+      border: 1px solid ${palette.gray_eb};
+      border-radius: 4px;
+      font-size: 16px;
+      outline: none;
+      ::placeholder {
+        color: ${palette.gray_76};
+      }
+    }
+    svg {
+      position: absolute;
+      right: 11px;
+      top: 16px;
+    }
+  }
 `;
 
 const SignUpModal: React.FC = () => {
-  return <Container>Sign Up</Container>;
+  return (
+    <Container>
+      <CloseXIcon className="modal-close-x-icon" />
+      <div className="input-wrapper">
+        <input placeholder="이메일 주소" type="email" name="email" />
+        <MailIcon />
+      </div>
+      <div className="input-wrapper">
+        <input placeholder="이름(예: 길동)" />
+        <PersonIcon />
+      </div>
+      <div className="input-wrapper">
+        <input placeholder="성(예: 홍)" />
+        <PersonIcon />
+      </div>
+      <div className="input-wrapper">
+        <input placeholder="비밀번호 설정하기" type="password" />
+        <OpenedEyeIcon />
+      </div>
+    </Container>
+  );
 };
 
 export default SignUpModal;
