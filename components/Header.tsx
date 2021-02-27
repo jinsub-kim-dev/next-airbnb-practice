6cdca2fc6c1c16b3f709e310b5a381dac284a122
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2021-02-27 10:47:28 +0900

    헤더 로그인 회원가입 버튼 작업

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Link from "next/link";
 import AirbnbLogoIcon from "../public/static/svg/logo/logo.svg";
 import AirbnbLogoTextIcon from "../public/static/svg/logo/logo_text.svg";
+import palette from "../styles/palette";
 
 const Container = styled.div`
   position: sticky;
@@ -23,6 +24,36 @@ const Container = styled.div`
       margin-right: 6px;
     }
   }
+
+  /** 헤더 로그인 회원가입 버튼 */
+  .header-auth-buttons {
+    .header-sign-up-button {
+      height: 42px;
+      margin-right: 8px;
+      padding: 0 16px;
+      border: 0;
+      border-radius: 21px;
+      background-color: white;
+      cursor: pointer;
+      outline: none;
+      &:hover {
+        background-color: ${palette.gray_f7};
+      }
+    }
+    .header-login-button {
+      height: 42px;
+      padding: 0 16px;
+      border: 0;
+      box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.18);
+      border-radius: 21px;
+      background-color: white;
+      cursor: pointer;
+      outline: none;
+      &:hover {
+        box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.12);
+      }
+    }
+  }
 `;
 
 const Header: React.FC = () => {
@@ -34,6 +65,14 @@ const Header: React.FC = () => {
           <AirbnbLogoTextIcon />
         </a>
       </Link>
+      <div className="header-auth-buttons">
+        <button type="button" className="header-sign-up-button">
+          회원가입
+        </button>
+        <button type="button" className="header-login-button">
+          로그인
+        </button>
+      </div>
     </Container>
   );
 };
