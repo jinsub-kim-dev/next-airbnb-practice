a7c1e6631640deadbb4a5a4c7f5ea2a32ba2b1a1
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2021-02-27 13:49:37 +0900

    모달 인풋 공통 컴포넌트 작업

--- components/auths/SignUpModal.tsx
+++ components/auths/SignUpModal.tsx
@@ -5,10 +5,12 @@ import MailIcon from "../../public/static/svg/auth/mail.svg";
 import PersonIcon from "../../public/static/svg/auth/person.svg";
 import OpenedEyeIcon from "../../public/static/svg/auth/opened_eye.svg";
 import ClosedEyeIcon from "../../public/static/svg/auth/closed_eye.svg";
+import Input from "../common/Input";
 import palette from "../../styles/palette";
 
 const Container = styled.form`
   width: 568px;
+  height: 614px;
   padding: 32px;
   background-color: white;
   z-index: 11;
@@ -22,24 +24,6 @@ const Container = styled.form`
   .input-wrapper {
     position: relative;
     margin-bottom: 16px;
-    input {
-      position: relative;
-      width: 100%;
-      height: 46px;
-      padding: 0 44px 0 11px;
-      border: 1px solid ${palette.gray_eb};
-      border-radius: 4px;
-      font-size: 16px;
-      outline: none;
-      ::placeholder {
-        color: ${palette.gray_76};
-      }
-    }
-    svg {
-      position: absolute;
-      right: 11px;
-      top: 16px;
-    }
   }
 `;
 
@@ -48,20 +32,20 @@ const SignUpModal: React.FC = () => {
     <Container>
       <CloseXIcon className="modal-close-x-icon" />
       <div className="input-wrapper">
-        <input placeholder="이메일 주소" type="email" name="email" />
-        <MailIcon />
+        <Input placeholder="이메일 주소" type="email" icon={<MailIcon />} />
       </div>
       <div className="input-wrapper">
-        <input placeholder="이름(예: 길동)" />
-        <PersonIcon />
+        <Input placeholder="이름(예: 길동)" icon={<PersonIcon />} />
       </div>
       <div className="input-wrapper">
-        <input placeholder="성(예: 홍)" />
-        <PersonIcon />
+        <Input placeholder="성(예: 홍)" icon={<PersonIcon />} />
       </div>
       <div className="input-wrapper">
-        <input placeholder="비밀번호 설정하기" type="password" />
-        <OpenedEyeIcon />
+        <Input
+          placeholder="비밀번호 설정하기"
+          type="password"
+          icon={<OpenedEyeIcon />}
+        />
       </div>
     </Container>
   );
