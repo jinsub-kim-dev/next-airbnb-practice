c33a2c3aed3a19db83f3f08150d232cc2606fdfa
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2021-03-01 20:51:46 +0900

    건물 유형 셀렉터 작업

--- store/registerRoom.ts
+++ store/registerRoom.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type RegisterRoomState = {
   largeBuildingType: string | null;
@@ -7,22 +7,38 @@ type RegisterRoomState = {
   isSetUpForGuest: boolean | null;
 };
 
-// 초기 상태
+//* 초기 상태
 const initialState: RegisterRoomState = {
-  // 건물 유형 큰 범주
+  //* 건물 유형 큰 범주
   largeBuildingType: null,
-  // 건물 유형
+  //* 건물 유형
   buildingType: null,
-  // 숙소 유형
+  //* 숙소 유형
   roomType: null,
-  // 게스트만을 위해 만들어진 숙소인가
+  //* 게스트만을 위해 만들어진 숙소인가
   isSetUpForGuest: null,
 };
 
 const registerRoom = createSlice({
   name: "registerRoom",
   initialState,
-  reducers: {},
+  reducers: {
+    //* 큰 건물 유형 변경하기
+    setLargeBuildingType(state, action: PayloadAction<string>) {
+      if (action.payload === "") {
+        state.largeBuildingType = null;
+      }
+      state.largeBuildingType = action.payload;
+      return state;
+    },
+    setBuildingType(state, action: PayloadAction<string>) {
+      if (action.payload === "") {
+        state.buildingType = null;
+      }
+      state.buildingType = action.payload;
+      return state;
+    },
+  },
 });
 
 export const registerRoomActions = { ...registerRoom.actions };
