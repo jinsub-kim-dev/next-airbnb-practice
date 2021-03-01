dcc95078d27a55302d8640ed26e1c76dd9ae0dc0
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2021-03-01 15:26:55 +0900

    유저 메뉴 작업

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,6 +10,7 @@ import HamburgerIcon from "../public/static/svg/header/hamburger.svg";
 import { useDispatch } from "react-redux";
 import { authActions } from "../store/auth";
 import AuthModal from "./auths/AuthModal";
+import OutsideClickHandler from "react-outside-click-handler";
 
 const Container = styled.div`
   position: sticky;
@@ -82,10 +83,47 @@ const Container = styled.div`
       border-radius: 50%;
     }
   }
+
+  /** react-outside-click-handler div */
+  .header-logo-wrapper + div {
+    position: relative;
+  }
+
+  .header-usermenu {
+    position: absolute;
+    right: 0;
+    top: 52px;
+    width: 240px;
+    padding: 8px 0;
+    box-shadow: 0 2px 16px rgba(0, 0, 0, 0.12);
+    border-radius: 8px;
+    background-color: white;
+    li {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      height: 42px;
+      padding: 0 16px;
+      cursor: pointer;
+      &:hover {
+        background-color: ${palette.gray_f7};
+      }
+    }
+    .header-usermenu-divider {
+      width: 100%;
+      height: 1px;
+      margin: 8px 0;
+      background-color: ${palette.gray_dd};
+    }
+  }
 `;
 
 const Header: React.FC = () => {
   const { openModal, ModalPortal, closeModal } = useModal();
+
+  // 유저 메뉴 열고, 닫힘 여부
+  const [isUsermenuOpened, setIsUsermenuOpened] = useState(false);
+
   const user = useSelector((state) => state.user);
 
   const dispatch = useDispatch();
@@ -123,14 +161,45 @@ const Header: React.FC = () => {
         </div>
       )}
       {user.isLogged && (
-        <button className="header-user-profile" type="button">
-          <HamburgerIcon />
-          <img
-            src={user.profileImage}
-            className="header-user-profile-image"
-            alt=""
-          />
-        </button>
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            if (isUsermenuOpened) {
+              setIsUsermenuOpened(false);
+            }
+          }}
+        >
+          <button
+            className="header-user-profile"
+            type="button"
+            onClick={() => setIsUsermenuOpened(!isUsermenuOpened)}
+          >
+            <HamburgerIcon />
+            <img
+              src={user.profileImage}
+              className="header-user-profile-image"
+              alt=""
+            />
+          </button>
+          {isUsermenuOpened && (
+            <ul className="header-usermenu">
+              <li>숙소 관리</li>
+              <Link href="/room/register/building">
+                <a
+                  role="presentation"
+                  onClick={() => {
+                    setIsUsermenuOpened(false);
+                  }}
+                >
+                  <li>숙소 등록하기</li>
+                </a>
+              </Link>
+              <div className="header-usermenu-divider" />
+              <li role="presentation" onClick={() => {}}>
+                로그아웃
+              </li>
+            </ul>
+          )}
+        </OutsideClickHandler>
       )}
       <ModalPortal>
         <AuthModal closeModal={closeModal} />
--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -11,6 +11,10 @@ const globalStyle = css`
     font-family: Noto sans, Noto Sans KR;
     color: ${palette.black};
   }
+  a {
+    text-decoration: none;
+    color: ${palette.black};
+  }
 `;
 
 const GlobalStyle = createGlobalStyle`
