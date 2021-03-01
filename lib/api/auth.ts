9031e0cf736e9f0ac4efd03781472df827d14fae
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2021-03-01 15:11:31 +0900

    로그인 유지 작업

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from ".";
 import { UserType } from "../../types/user";
 
 // 회원가입 body
@@ -17,3 +17,6 @@ export const signupAPI = (body: SignUpAPIBody) =>
 // 로그인 api
 export const loginAPI = (body: { email: string; password: string }) =>
   axios.post<UserType>("/api/auth/login", body);
+
+// 쿠키의 access_token의 유저 정보 받아오는 api
+export const meAPI = () => axios.get<UserType>("/api/auth/me");
