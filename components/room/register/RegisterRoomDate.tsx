35e8fd8e97330447b0fcce22c018c25cc1a1b9ef
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2021-03-06 01:20:10 +0900

    공통 DatePicker 적용 작업

--- components/room/register/RegisterRoomDate.tsx
+++ components/room/register/RegisterRoomDate.tsx
@@ -6,6 +6,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import { useSelector } from "../../../store";
 import { useDispatch } from "react-redux";
 import { registerRoomActions } from "../../../store/registerRoom";
+import RegisterRoomFooter from "./RegisterRoomFooter";
 
 const Container = styled.div`
   padding: 62px 30px 100px;
@@ -19,10 +20,45 @@ const Container = styled.div`
     color: ${palette.gray_76};
     margin-bottom: 6px;
   }
+
+  .register-room-date-wrapper {
+    display: flex;
+    align-items: center;
+    label {
+      span {
+        display: block;
+        margin-bottom: 8px;
+      }
+    }
+    input {
+      display: block;
+      position: relative;
+      width: 100%;
+      height: 46px;
+      padding: 0 11px;
+      border: 1px solid ${palette.gray_eb};
+      border-radius: 4px;
+      font-size: 16px;
+      outline: none;
+      &::placeholder {
+        color: ${palette.gray_76};
+      }
+      &:focus {
+        border-color: ${palette.dark_cyan};
+      }
+    }
+    .register-room-start-date {
+      margin-right: 20px;
+    }
+  }
 `;
 
 const RegisterRoomDate: React.FC = () => {
   const startDate = useSelector((state) => state.registerRoom.startDate);
+  const endDate = useSelector((state) => state.registerRoom.endDate);
+
+  const dateStartDate = startDate ? new Date(startDate) : null;
+  const dateEndDate = endDate ? new Date(endDate) : null;
 
   const dispatch = useDispatch();
 
@@ -33,14 +69,40 @@ const RegisterRoomDate: React.FC = () => {
       registerRoomActions.setStartDate(date ? date.toISOString() : null),
     );
   };
+  //* 예약 종료 날짜 변경 시
+  const onChangeEndDate = (date: Date | null) => {
+    console.log(date);
+    dispatch(registerRoomActions.setEndDate(date ? date.toISOString() : null));
+  };
 
   return (
     <Container>
       <h2>예약 가능 여부 설정하기</h2>
       <h3>11단계</h3>
-      <DatePicker
-        selected={startDate ? new Date(startDate) : null}
-        onChange={onChangeStartDate}
+      <div className="register-room-date-wrapper">
+        <div className="register-room-start-date">
+          <label>
+            <span>예약 시작일</span>
+            <DatePicker
+              selected={startDate ? new Date(startDate) : null}
+              onChange={onChangeStartDate}
+            />
+          </label>
+        </div>
+
+        <div className="register-room-end-date">
+          <label>
+            <span>예약 마감일</span>
+            <DatePicker
+              selected={endDate ? new Date(endDate) : null}
+              onChange={onChangeEndDate}
+            />
+          </label>
+        </div>
+      </div>
+      <RegisterRoomFooter
+        prevHref="/room/register/price"
+        nextHref="/room/register/checklist"
       />
     </Container>
   );
