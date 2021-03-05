dd74d5ead95099e01f4474293981aa1de26442ed
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2021-03-05 21:54:46 +0900

    숙소 가격 작업

--- components/room/register/RegisterRoomPrice.tsx
+++ components/room/register/RegisterRoomPrice.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
+import { makeMoneyString } from "../../../lib/utils";
 import { useSelector } from "../../../store";
+import { registerRoomActions } from "../../../store/registerRoom";
 import palette from "../../../styles/palette";
 import Input from "../../common/Input";
 import RegisterRoomFooter from "./RegisterRoomFooter";
 
 const Container = styled.div`
   padding: 62px 30px 100px;
+  width: 445px;
   h2 {
     font-size: 19px;
     font-weight: 800;
@@ -25,11 +28,30 @@ const RegisterRoomPrice: React.FC = () => {
 
   const price = useSelector((state) => state.registerRoom.price);
 
+  //* 금액 변경시
+  const onChangePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+
+    //? 인풋 값이 비워지면 price를 0으로 변경
+    if (!input) {
+      dispatch(registerRoomActions.setPrice(0));
+    }
+
+    const numberPrice = Number(input.replace(/,/g, ""));
+    if (numberPrice) {
+      dispatch(registerRoomActions.setPrice(numberPrice));
+    }
+  };
+
   return (
     <Container>
       <h2>숙소 요금 설정하기</h2>
       <h3>10단계</h3>
-      <Input label="기본요금" value={String(price)} />
+      <Input
+        label="기본요금"
+        value={makeMoneyString(String(price))}
+        onChange={onChangePrice}
+      />
       <RegisterRoomFooter
         prevHref="/room/register/title"
         nextHref="/room/register/date"
--- lib/utils.ts
+++ lib/utils.ts
@@ -21,3 +21,12 @@ export const getNumber = (string: string) => {
   }
   return null;
 };
+
+//* 금액을 입력하면 금액에 ,를 넣어주는 함수
+export const makeMoneyString = (input: string) => {
+  const amountString = input.replace(/[^0-9]/g, "");
+  if (amountString) {
+    return parseInt(amountString, 10).toLocaleString();
+  }
+  return "";
+};
