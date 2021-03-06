94d8b51a92ad33ff731e1c7ceb261b657e966479
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2021-03-06 22:34:24 +0900

    메인 페이지 레이아웃 스타일링 작업

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
+import { NextPage } from "next";
 import React from "react";
 import styled from "styled-components";
+import Home from "../components/home/Home";
 
 const Container = styled.div`
   font-size: 21px;
   color: gray;
 `;
 
-const index: React.FC = () => {
-  return <div>Hello World</div>;
+const index: NextPage = () => {
+  return <Home />;
 };
 
 export default index;
