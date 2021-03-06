7151045e89d2765852549b9a4f1d743ed48812a0
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2021-03-06 23:13:50 +0900

    숙소 위치 검색 작업

--- components/home/searchRoomBar/SearchRoomBarLocation.tsx
+++ components/home/searchRoomBar/SearchRoomBarLocation.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import OutsideClickHandler from "react-outside-click-handler";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
+import { useSelector } from "../../../store";
+import { searchRoomActions } from "../../../store/searchRoom";
 import palette from "../../../styles/palette";
 
 const Container = styled.div`
@@ -12,10 +16,95 @@ const Container = styled.div`
   &:hover {
     border-color: ${palette.gray_dd};
   }
+  .search-room-bar-location-texts {
+    position: absolute;
+    width: calc(100% - 40px);
+    top: 16px;
+    left: 20px;
+    .search-room-bar-location-label {
+      font-size: 10px;
+      font-weight: 800;
+      margin-bottom: 4px;
+    }
+    input {
+      width: 100%;
+      border: 0;
+      font-size: 14px;
+      font-weight: 600;
+      outline: none;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      &::placeholder {
+        font-size: 14px;
+        opacity: 0.7;
+      }
+    }
+  }
+  .search-room-bar-location-results {
+    position: absolute;
+    background-color: white;
+    top: 78px;
+    width: 500px;
+    padding: 16px 0;
+    box-shadow: 0 6px 20px rgba(0, 0, 0, 0.2);
+    border-radius: 32px;
+    cursor: default;
+    overflow: hidden;
+    z-index: 10;
+    li {
+      display: flex;
+      align-items: center;
+      height: 64px;
+      padding: 8px 32px;
+      cursor: pointer;
+      &:hover {
+        background-color: ${palette.gray_f7};
+      }
+    }
+  }
 `;
 
 const SearchRoomBarLocation: React.FC = () => {
-  return <Container>위치</Container>;
+  const location = useSelector((state) => state.searchRoom.location);
+
+  const dispatch = useDispatch();
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  //* 위치 변경 Dispatch
+  const setLocationDispatch = (value: string) => {
+    dispatch(searchRoomActions.setLocation(value));
+  };
+  const [popupOpened, setPopupOpened] = useState(false);
+
+  const onClickInput = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+    setPopupOpened(true);
+  };
+
+  return (
+    <Container onClick={onClickInput}>
+      <OutsideClickHandler onOutsideClick={() => setPopupOpened(false)}>
+        <div className="search-room-bar-location-texts">
+          <p className="search-room-bar-location-label">인원</p>
+          <input
+            value={location}
+            onChange={(e) => setLocationDispatch(e.target.value)}
+            placeholder="어디로 여행 가세요?"
+            ref={inputRef}
+          />
+        </div>
+        {popupOpened && (
+          <ul className="search-room-bar-location-results">
+            <li>근처 추천 장소</li>
+          </ul>
+        )}
+      </OutsideClickHandler>
+    </Container>
+  );
 };
 
 export default SearchRoomBarLocation;
