d710416c095f6be4d394ac9960a69f19dd859730
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2021-03-07 11:49:42 +0900

    숙소 검색 예약 날짜 인풋

--- components/home/searchRoomBar/SearchRoomCheckInDate.tsx
+++ components/home/searchRoomBar/SearchRoomCheckInDate.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import palette from "../../../styles/palette";
+import useSearchRoom from "../../../hooks/useSearchRoomDate";
+import DatePicker from "../../common/DatePicker";
 
 const Container = styled.div`
   position: relative;
@@ -12,10 +14,63 @@ const Container = styled.div`
   &:hover {
     border-color: ${palette.gray_dd};
   }
+  .search-room-bar-date-label {
+    font-size: 10px;
+    font-weight: 800;
+    margin-bottom: 4px;
+    position: absolute;
+    z-index: 1;
+    left: 20px;
+    top: 16px;
+  }
+  input {
+    width: 100%;
+    height: 100%;
+    padding: 20px 0 0 20px;
+    border: 0;
+    border-radius: 12px;
+    font-weight: 600;
+    outline: none;
+    cursor: pointer;
+  }
+  > div {
+    width: 100%;
+    height: 100%;
+    .react-datepicker-wrapper {
+      width: 100%;
+      height: 100%;
+      .react-datepicker__input-container {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .react-datepicker {
+      display: flex;
+    }
+  }
 `;
 
 const SearchRoomCheckInDate: React.FC = () => {
-  return <Container>체크인</Container>;
+  const { checkInDate, checkOutDate, setCheckInDateDispatch } = useSearchRoom();
+  const onChangeCheckInDate = (date: Date | null) =>
+    setCheckInDateDispatch(date);
+  return (
+    <Container>
+      <div>
+        <p className="search-room-bar-date-label">체크인</p>
+        <DatePicker
+          selected={checkInDate}
+          monthsShown={2}
+          onChange={onChangeCheckInDate}
+          selectsStart
+          startDate={checkInDate}
+          endDate={checkOutDate}
+          placeholderText="날짜 추가"
+          minDate={new Date()}
+        />
+      </div>
+    </Container>
+  );
 };
 
 export default SearchRoomCheckInDate;
--- components/home/searchRoomBar/SearchRoomCheckOutDate.tsx
+++ components/home/searchRoomBar/SearchRoomCheckOutDate.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import palette from "../../../styles/palette";
+import useSearchRoom from "../../../hooks/useSearchRoomDate";
+import DatePicker from "../../common/DatePicker";
 
 const Container = styled.div`
   position: relative;
@@ -12,10 +14,71 @@ const Container = styled.div`
   &:hover {
     border-color: ${palette.gray_dd};
   }
+  .search-room-bar-date-label {
+    font-size: 10px;
+    font-weight: 800;
+    margin-bottom: 4px;
+    position: absolute;
+    z-index: 1;
+    left: 20px;
+    top: 16px;
+  }
+  input {
+    width: 100%;
+    height: 100%;
+    padding: 20px 0 0 20px;
+    border: 0;
+    border-radius: 12px;
+    font-weight: 600;
+    outline: none;
+    cursor: pointer;
+  }
+  > div {
+    width: 100%;
+    height: 100%;
+    .react-datepicker-wrapper {
+      width: 100%;
+      height: 100%;
+      .react-datepicker__input-container {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .react-datepicker {
+      display: flex;
+    }
+  }
 `;
 
 const SearchRoomCheckOutDate: React.FC = () => {
-  return <Container>체크아웃</Container>;
+  const {
+    checkInDate,
+    checkOutDate,
+    setCheckOutDateDispatch,
+  } = useSearchRoom();
+
+  //* 체크아웃 날짜 변경 시
+  const onChangeCheckOutDate = (date: Date | null) =>
+    setCheckOutDateDispatch(date);
+
+  return (
+    <Container>
+      <div>
+        <p className="search-room-bar-date-label">체크아웃</p>
+        <DatePicker
+          selected={checkOutDate}
+          monthsShown={2}
+          onChange={onChangeCheckOutDate}
+          selectsEnd
+          popperPlacement="bottom-end"
+          startDate={checkInDate}
+          endDate={checkOutDate}
+          minDate={checkInDate}
+          placeholderText="날짜 추가"
+        />
+      </div>
+    </Container>
+  );
 };
 
 export default SearchRoomCheckOutDate;
